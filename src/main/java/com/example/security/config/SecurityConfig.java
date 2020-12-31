9d18fed6a5db89ac92c45551393a854b614af778
kiseok <kisa0828@naver.com>
2020-12-31 15:36:21 +0900

    SecurityExpressionHandler로 role 계층구조설정

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,21 +1,42 @@
 package com.example.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * .mvcMatchers("/User").hasRole("USER") 일 때 Admin은 유저이면서 admin롤을 갖는다
+     * 따라서 admin도 user role에 접근할 수 있어야하는데 이대로면 admin은 user role로 접근할 수 없다.
+     */
+    public SecurityExpressionHandler securityExpressionHandler() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        /**  게층구조를 설정해주고*/
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        /** 핸들러에 계층구조를 설정*/
+        return handler;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .mvcMatchers("/", "/info","/account/**").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin", "/dashBoard").hasRole("ADMIN")
-                .anyRequest().authenticated(); // 기타등등은 인증을 하면 접근할 수 있다
+                .mvcMatchers("/User").hasRole("USER")
+                .anyRequest().authenticated() // 기타등등은 인증을 하면 접근할 수 있다
+                .expressionHandler(securityExpressionHandler());
                 //.and() //and로 이어가거나 아니면 Request내용 이후 다른 내용을 추가할 때 새로 http로 시작해주거나
         http
                 .formLogin(); //로그아웃도 가능
